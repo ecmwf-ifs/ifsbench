175357b596d5deb5522a9f0b54c6a42f90c8253c
Michael Lange <Michael.Lange@ecmwf.int>
2025-06-02 12:52:39 +0000

    Perfplot: Add utilities to read preset from yaml file

--- ifsbench/command_line/perfplot.py
+++ ifsbench/command_line/perfplot.py
@@ -8,95 +8,49 @@
 """
 Plotting utility entry point for performance data plotting.
 """
+from pathlib import Path
 
 import click
+import yaml
 
 from ifsbench import DrHookRecord, GStatsRecord
 from ifsbench.plot import GroupedBarPlot, StackedBarPlot
-from ifsbench.logging import logger, DEBUG
+from ifsbench.logging import logger, DEBUG, warning
 
 
-preset_cpu_fused_ecphys = {
-    'Init': 'EC_PHYS_DRV_FIELD_INIT',
-    'Pre': 'EC_PHYS_DRV_PRE',
-    'Pre2': 'EC_PHYS_PRE',
-    'Pre3': 'CALLPAR_PRE',
-    'Rad Trans': 'CALLPAR_RADTRANS',
-    'Turbulence': 'CALLPAR_TURB',
-    'Convection': 'CALLPAR_CONV',
-    'Cloud': 'CALLPAR_CLOUD',
-    'WMS': 'CALLPAR_WMS',
-    'Diag': 'CALLPAR_DIAG_CTV',
-    'Chem': 'CALLPAR_CHEM',
-    'Rad Vis': 'CALLPAR_RADVIS',
-    'Surface': 'CALLPAR_SURFTS',
-    'Ozone': 'CALLPAR_O3CHEM',
-    'SLPhy': 'CALLPAR_SLPHY',
-    'Post1': 'CALLPAR_POST',
-    'Post2': 'EC_PHYS_POST',
-    'Deallo': 'EC_PHYS_DRV_DEALLO',
-}
+def preset_from_yaml(path):
+    """
+    Read "preset" mapping of group keys to DrHook timing labels
+    """
+    with Path(path).open(encoding='utf-8') as f:
+        preset_map = yaml.safe_load(f)
+    return preset_map
 
-preset_cpu_fused_ecphys2 = {
-    'Init': lambda d: d.get('EC_PHYS_DRV_FIELD_INIT'),
-    'Pre': lambda d: sum(d.get(['EC_PHYS_DRV_PRE', 'EC_PHYS_PRE', 'CALLPAR_PRE'])),
-    'Rad Trans': lambda d: d.get('CALLPAR_RADTRANS'),
-    'Turbulence': lambda d: d.get('CALLPAR_TURB'),
-    'Convection': lambda d: d.get('CALLPAR_CONV'),
-    'Cloud': lambda d: d.get('CALLPAR_CLOUD'),
-    'WMS': lambda d: d.get('CALLPAR_WMS'),
-    'Diag': lambda d: d.get('CALLPAR_DIAG_CTV'),
-    'Chem': lambda d: d.get('CALLPAR_CHEM'),
-    'Rad Vis': lambda d: d.get('CALLPAR_RADVIS'),
-    'Surface': lambda d: d.get('CALLPAR_SURFTS'),
-    'Ozone': lambda d: d.get('CALLPAR_O3CHEM'),
-    'SLPhy': lambda d: d.get('CALLPAR_SLPHY'),
-    'Post': lambda d: sum(d.get(['CALLPAR_POST', 'EC_PHYS_POST', 'EC_PHYS_DRV_DEALLO'])),
-}
 
-preset_gpu_ecphys = {
-    'Init': 'ECPHYS_FIELD_INIT',
-    'Pre': 'EC_PHYS_FC_PRE',
-    'Rad Trans': 'EC_PHYS_FC_RADTRANS',
-    'Turbulence': 'TURBULENCE_LAYER_LOKI',
-    'Convection': 'CONVECTION_LAYER_LOKI',
-    'Cloud': 'CLOUD_LAYER_LOKI',
-    'WMS': 'EC_PHYS_FC_WMS',
-    'Diag': 'EC_PHYS_FC_DIAG_CTV',
-    'Chem' : 'EC_PHYS_FC_CHEM',
-    # 'Chem + Rad Vis' : 'EC_PHYS_FC_CHEM',
-    'Surface': 'EC_PHYS_FC_SURFTS',
-    'Ozone': 'EC_PHYS_FC_O3CHEM',
-    'SLPhy': 'EC_PHYS_FC_SLPHY',
-    # 'SLPhy + Qnegat': 'EC_PHYS_FC_SLPHY',
-    'Post1': 'EC_PHYS_FC_POST',
-    'Post2': 'EC_PHYS_FC_POST2',
-    'Deallo': 'EC_PHYS_FC_DEALLO',
-    'Pullback': 'EC_PHYS_FC_PULLBACK',
-    'Mem-save PB': 'CONVECTION_PULLBACK',
-}
+def data_from_preset(drhook, preset, groups):
+    """
+    Construct an array of timing values, summing any list of grouped keys.
+    """
 
-preset_gpu_ecphys2 = {
-    'Init': lambda d: d.get('ECPHYS_FIELD_INIT'),
-    'Pre': lambda d: d.get('EC_PHYS_FC_PRE'),
-    'Rad Trans': lambda d: d.get('EC_PHYS_FC_RADTRANS'),
-    'Turbulence': lambda d: sum(d.get(['GWDRAG_LAYER_LOKI', 'TURBULENCE_LAYER_LOKI'])),
-    'Convection': lambda d: d.get('CONVECTION_LAYER_LOKI'),
-    'Cloud': lambda d: d.get('CLOUD_LAYER_LOKI'),
-    'WMS': lambda d: d.get('EC_PHYS_FC_WMS'),
-    'Diag': lambda d: d.get('EC_PHYS_FC_DIAG_CTV'),
-    'Chem' : lambda d: d.get('EC_PHYS_FC_CHEM'),
-    # 'Chem + Rad Vis' : 'EC_PHYS_FC_CHEM',
-    'Surface': lambda d: d.get('EC_PHYS_FC_SURFTS'),
-    'Ozone': lambda d: d.get('EC_PHYS_FC_O3CHEM'),
-    'SLPhy': lambda d: d.get('EC_PHYS_FC_SLPHY'),
-    # 'SLPhy + Qnegat': 'EC_PHYS_FC_SLPHY',
-    'Post': lambda d: sum(d.get(['EC_PHYS_FC_POST', 'EC_PHYS_FC_POST2', 'EC_PHYS_FC_DEALLO'])),
-    # 'Post2': lambda d: d.get('EC_PHYS_FC_POST2'),
-    # 'Deallo': lambda d: d.get('EC_PHYS_FC_DEALLO'),
-    'Pullback': lambda d: d.get('EC_PHYS_FC_PULLBACK'),
-    'Mem-save PB': lambda d: d.get('CONVECTION_PULLBACK'),
-}
+    data = []
+    for k in groups:
+        if lbl := preset.get(k):
+            val = drhook.get(lbl, fill=0.)
+
+            if isinstance(lbl, str):
+                # Ensure lists from here
+                val, lbl = [val], [lbl]
+
+            for l, v in zip(lbl, val):
+                if v == 0.:
+                    warning(f'[Perfplot] Could not find label {l} in DR_HOOK timings')
+
+            data.append( sum(val) )
+        else:
+            warning(f'[Perfplot] Could not find group key {k} in preset')
+            data.append( 0.0 )
+
+    return data
 
 
 @click.group()
@@ -110,8 +64,16 @@ def perfplot():
     help='Paths to CPU data sets to add to bar plot'
 )
 @click.option(
+    '--cpu-preset', '-cp', multiple=True, type=click.Path(),
+    help='Paths to group preset definition YAML file'
+)
+@click.option(
     '--gpu-data', '-g', multiple=True, type=click.Path(),
     help='Paths to GPU data sets to add to bar plot'
+)
+@click.option(
+    '--gpu-preset', '-gp', multiple=True, type=click.Path(),
+    help='Paths to group preset definition YAML file'
 )
 @click.option(
     '--output', default='plot.pdf', type=click.Path(),
@@ -125,7 +87,7 @@ def perfplot():
     '--verbose', '-v', is_flag=True, default=False,
     help='Enable verbose (debugging) output.'
 )
-def plot_ecphysics_stacked(cpu_data, gpu_data, output, cmap, verbose):
+def plot_ecphysics_stacked(cpu_data, cpu_preset, gpu_data, gpu_preset, output, cmap, verbose):
     """
     Plot EC-physics performance as a stacked barplot for comparing difference runs.
     """
@@ -141,31 +103,27 @@ def plot_ecphysics_stacked(cpu_data, gpu_data, output, cmap, verbose):
     groups += ['Other']
     plot = StackedBarPlot(groups, cmap=cmap)
 
-    for path in cpu_data:
-        # preset = preset_cpu_fused_ecphys
-        preset = preset_cpu_fused_ecphys2
+    for path, preset_path in zip(cpu_data, cpu_preset):
         drhook = DrHookRecord.from_raw(path)
         gstats = GStatsRecord.from_file(path)
 
-        # keys = [preset.get(k, 'N/A') for k in groups[:-1]]
-        # data = drhook.get(keys, fill=0.0)
-        data = [preset[k](drhook) if k in preset else 0. for k in groups[:-1]]
+        preset = preset_from_yaml(preset_path)
+
+        data = data_from_preset(drhook=drhook, preset=preset, groups=groups[:-1])
 
         # Add "other" as the difference to the EC_PHYS gstats label
         data += [gstats.total_ecphys - sum(data)]
 
         plot.add_stack(data, label='CPU-Fused')
 
-        # from IPython import embed; embed()
+    for path, preset_path in zip(gpu_data, gpu_preset):
 
-    for path in gpu_data:
-        preset = preset_gpu_ecphys2
         drhook = DrHookRecord.from_raw(path)
         gstats = GStatsRecord.from_file(path)
 
-        # keys = [preset.get(k, 'NA') for k in groups[:-1]]
-        # data = drhook.get(keys, fill=0.0)
-        data = [preset[k](drhook) if k in preset else 0. for k in groups[:-1]]
+        preset = preset_from_yaml(preset_path)
+
+        data = data_from_preset(drhook=drhook, preset=preset, groups=groups[:-1])
 
         # Add "other" as the difference to the EC_PHYS gstats label
         data += [gstats.total_ecphys - sum(data)]
