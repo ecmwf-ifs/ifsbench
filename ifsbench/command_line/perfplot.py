e45ddd419ceb92f373e97c84c72d5b80d9a79714
Michael Staneker <michael.staneker@ecmwf.int>
2025-10-15 16:00:00 +0200

    perfplot/barplot: re-introduce and extend capabilities

--- ifsbench/command_line/perfplot.py
+++ ifsbench/command_line/perfplot.py
@@ -33,20 +33,23 @@ def data_from_preset(drhook, preset, groups, do_sums=True):
     Construct an array of timing values, summing any list of grouped keys.
     """
 
+    def flatten(values):
+        flattened = []
+        if isinstance(values, dict):
+            for key, vals in values.items():
+                flattened.extend(vals)
+        return flattened
+
     data = []
     for k in groups:
         if lbl := preset.get(k):
             val = drhook.get(lbl, fill=0.)
 
-            if isinstance(lbl, str):
-                # Ensure lists from here
-                val, lbl = [val], [lbl]
-
             for l, v in zip(lbl, val):
                 if v == 0.:
                     warning(f'[Perfplot] Could not find label {l} in DR_HOOK timings')
 
-            data.append( sum(val) if do_sums else val )
+            data.append( sum(flatten(val)) if do_sums else val )
         else:
             warning(f'[Perfplot] Could not find group key {k} in preset')
             data.append( 0.0 )
@@ -57,6 +60,60 @@ def data_from_preset(drhook, preset, groups, do_sums=True):
 @click.group()
 def perfplot():
     pass
+
+@perfplot.command('overview-stacked')
+@click.option(
+    '--config', '-c', type=click.Path(),
+    help='Paths to plot configuration YAML file'
+)
+@click.option(
+    '--output', '-o', default='plot.pdf', type=click.Path(),
+    help='Filename of the output plot'
+)
+@click.option(
+    '--cmap', '-cm', default='viridis',
+    help='Colormap to use for different component bars'
+)
+@click.option(
+    '--verbose', '-v', is_flag=True, default=False,
+    help='Enable verbose (debugging) output.'
+)
+def plot_overview_stacked(config, output, cmap, verbose):
+    """
+    Plot EC-physics performance as a stacked barplot for comparing difference runs.
+    """
+    metric = 'avgTimeTotal'
+
+    if verbose:
+        logger.setLevel(DEBUG)
+
+    config = config_from_yaml(config)
+
+    cs = ['darkolivegreen', 'goldenrod', 'darkslategray', 'darkcyan', 'firebrick', 'mediumblue', 'black', 'grey']
+    groups = ['PHYSICS', 'RADIATION', 'DYNAMICS', 'SEMI IMPL', 'TRANS', 'WAVE', 'IO', 'OTHER']
+
+    plot = StackedBarPlot(groups, colors=cs, nruns=len(config['runs']))
+
+    for run in config['runs']:
+        label = run['label']
+        path = Path(run['path'])
+        preset = run['preset']
+
+        drhook = DrHookRecord.from_raw(path)
+        gstats = GStatsRecord.from_file(path)
+
+        data = []
+        # Add "other" as the difference to the EC_PHYS gstats label
+        data += [gstats.total_ecphys, gstats.total_radiation, gstats.total_gp_dynamics,
+                 gstats.total_semi_lag, gstats.total_trans, gstats.total_wave, gstats.total_io]
+        others = gstats.total_time_execution - sum(data)
+        if others < 0:
+            raise ValueError
+        data += [others] # [gstats.total_time_execution - sum(data)]
+
+        plot.add_stack(data, label=label, groups=groups, text=True) # ['ECPHYS', 'WAVE', 'TRANS', 'OTHER'])
+
+    plot.plot(filename=output)
 
 
 @perfplot.command('ecphys-stacked')
@@ -127,6 +184,7 @@ def plot_ecphysics_compare(config, output, verbose):
     """
     Plot the EC-physics performance breakdown per component.
     """
+    
     metric = 'avgTimeTotal'
 
     if verbose:
@@ -134,8 +192,9 @@ def plot_ecphysics_compare(config, output, verbose):
 
     config = config_from_yaml(config)
     groups = config['groups']
+    group_colors = config['group_colors']
 
-    plot = GroupedBarPlot(groups=groups, figsize=(12,7))
+    plot = GroupedBarPlot(groups=groups, group_colors=group_colors, figsize=(12,7))
 
     for run in config['runs']:
         label = run['label']
@@ -150,3 +209,72 @@ def plot_ecphysics_compare(config, output, verbose):
         plot.add_group(data, label=label)
 
     plot.plot(filename=output)
+
+
+@perfplot.command('ecphys-detail-stacked')
+@click.option(
+    '--config', '-c', type=click.Path(),
+    help='Paths to plot configuration YAML file'
+)
+@click.option(
+    '--output', '-o', default='plot.pdf', type=click.Path(),
+    help='Filename of the output plot'
+)
+@click.option(
+    '--verbose', '-v', is_flag=True, default=False,
+    help='Enable verbose (debugging) output.'
+)
+def plot_ecphysics_detail_stacked(config, output, verbose):
+    """
+    Plot the EC-physics performance breakdown per component.
+    """
+
+    metric = 'avgTimeTotal'
+
+    if verbose:
+        logger.setLevel(DEBUG)
+
+    config = config_from_yaml(config)
+    groups = config['groups']
+    groups.append('OTHERS')
+    group_colors = config['group_colors']
+    group_colors.append('grey')
+
+    # plot = GroupedBarPlot(groups=groups, figsize=(12,7))
+    plot = StackedBarPlot(groups, group_colors=group_colors, nruns=len(config['runs']))
+
+    for run in config['runs']:
+        label = run['label']
+        path = Path(run['path'])
+        preset = run['preset']
+
+        drhook = DrHookRecord.from_raw(path)
+        gstats = GStatsRecord.from_file(path)
+
+        data = data_from_preset(drhook=drhook, preset=preset, groups=groups[:-1], do_sums=False)
+
+        data.append({"_": [gstats.total_ecphys]})
+        plot.add_group(data, label=label, groups=groups)
+
+    plot.plot(filename=output, save=False)
+
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as mpatches
+
+
+    handles = [mpatches.Patch(color=group_colors[i], label=groups[i]) for i in range(len(groups))]
+    handles_kind = []
+    handles_kind.append(mpatches.Patch(label='EXECUTE', facecolor='white', edgecolor='k'))
+    handles_kind.append(mpatches.Patch(label='OFFLOAD', hatch='//', facecolor='white', edgecolor='k'))
+    handles_kind.append(mpatches.Patch(label='PULLBACK', hatch='||||', facecolor='white', edgecolor='k'))
+    handles_kind.append(mpatches.Patch(label='SETUP', hatch='\\\\', facecolor='white', edgecolor='k'))
+
+    legend1 = plt.legend(handles=handles, loc='upper center', bbox_to_anchor=(0.5, 1.105),
+        fancybox=True, shadow=True, ncol=8, fontsize=11)
+    legend2 = plt.legend(handles=handles_kind, loc='upper center', bbox_to_anchor=(0.5, 1.025),
+        fancybox=True, shadow=True, ncol=4, fontsize=13)
+    plt.gca().add_artist(legend1)
+    plt.gca().add_artist(legend2)
+    extra_artists = [legend1, legend2]
+    
+    plot.save(filename=output)
--- ifsbench/plot/barplot.py
+++ ifsbench/plot/barplot.py
@@ -27,8 +27,9 @@ class GroupedBarPlot:
         Names of the groups by which to cluster bars.
     """
 
-    def __init__(self, groups, nruns=2, figsize=(12,9)):
+    def __init__(self, groups, group_colors, nruns=2, figsize=(12,9)):
         self.groups = groups
+        self.group_colors = group_colors
         self.x = np.arange(len(self.groups))
 
         self.fig, self.ax = plt.subplots(figsize=figsize, layout='constrained')
@@ -40,7 +41,7 @@ class GroupedBarPlot:
         info(f'[Perfplot] Creating GroupedBarPlot with {len(self.groups)} groups '
              f'[figsize={self.figsize}, width={self.width:.2f}]')
 
-    def add_group(self, values, label, hatch=None):
+    def add_group(self, values, label, hatch_dic={'_': '', 'execute': '', 'offload': '//', 'setup': '\\'}):
         """
         Add a bar to each group for a given set of values.
 
@@ -51,12 +52,19 @@ class GroupedBarPlot:
         label : str
             Label to appear in the legend for all entries
         """
-        hatch = ['', '//', '\\'] if not hatch else hatch
-
         assert len(values) == len(self.groups)
 
         # Figure the max set of stacked values and create a filled matrix
-        maxlen = max(len(v) if isinstance(v, Iterable) else 1 for v in values)
+        maxlen = max(len(v) if isinstance(v, (Iterable, dict)) else 1 for v in values)
+        # 'unpack' the values dict
+        _values = []
+        for elem in values:
+            nested_vals = []
+            for key, val in elem.items():
+                nested_vals.append(sum(val))
+            _values.append(nested_vals)
+        #
+        values = _values
         fill_values = np.asarray([
             [v[i] if isinstance(v, Iterable) and len(v) > i else 0. for v in values]
             for i in range(maxlen)
@@ -73,7 +81,7 @@ class GroupedBarPlot:
             prev = cum_values[i-1,:] if i >= 1 else [0. for _ in values]
             rects = self.ax.bar(
                 self.x + offset, fill_values[i,:], bottom=prev,
-                width=self.width, label=label, hatch=hatch[i], color=color
+                width=self.width# , label=label#, hatch=hatch[i] # , color=color
             )
             # Record most recent color so that we can keep it homogenous
             color = rects.patches[-1].get_facecolor()
@@ -119,16 +127,21 @@ class StackedBarPlot:
         Name of the colormap to use, as defined in matplotlib.
     """
 
-    def __init__(self, groups, cmap='viridis', figsize=(12,9)):
+    def __init__(self, groups, group_colors=None, cmap='viridis', colors=None, figsize=(12,9), nruns=2):
         self.groups = groups
+        self.group_colors = group_colors
 
         self.fig, self.ax = plt.subplots(figsize=figsize)
+        self.nruns = nruns
 
         # Define and discretise colormap
-        self.cmap = mpl.colormaps[cmap]
-        self.colors = self.cmap(np.linspace(0, 1, len(groups)))
+        if colors is None:
+            self.cmap = mpl.colormaps[cmap]
+            self.colors = self.cmap(np.linspace(0, 1, len(groups)))
+        else:
+            self.colors = colors
 
-    def add_stack(self, values, label):
+    def add_stack(self, values, label, groups=None, text=False):
         """
         Add stacked bar data for defined groups from a single run.
 
@@ -139,16 +152,74 @@ class StackedBarPlot:
         label : str
             Label for the x-axis of this entry
         """
-        assert len(values) == len(self.groups)
+        if groups is None:
+            groups = self.groups
+
+        assert len(values) == len(groups)
 
         total = 0.
         for i, v in enumerate(values):
-            plt.bar(label, v, bottom=total, label=self.groups[i], color=self.colors[i])
+            bar = plt.bar(label, v, bottom=total, label=groups[i], color=self.colors[i])
             total += v
 
-        debug(dict(zip(self.groups, values)))
+        if text:
+            yval = total # bar.get_height()
+            plt.text(label, yval + 0.01 * yval, round(yval, 1), horizontalalignment='center') 
 
-    def plot(self, filename, title=None, ylabel=None, ylim=None):
+        debug(dict(zip(groups, values)))
+
+
+    def add_group(self, values, label, hatch_dic={'_': '', 'execute': '', 'offload': '//', 'setup': '\\'}, colors=None, groups=None):
+        """
+        Add a bar to each group for a given set of values.
+
+        Parameters
+        ----------
+        values : list of float
+            Values for the bar height for all group entries.
+        label : str
+            Label to appear in the legend for all entries
+        """
+
+        groups = groups or self.groups
+        assert len(values) == len(groups)
+
+        _colors = self.group_colors
+
+        # Figure the max set of stacked values and create a filled matrix
+        maxlen = max(len(v) if isinstance(v, (Iterable, dict)) else 1 for v in values)
+        combined_values = []
+        for elem in values:
+            combined_values.append({key: sum(val) for key, val in elem.items()})
+        fill_values = []
+        hatches = []
+        colors = []
+        for i_elem, elem in enumerate(combined_values):
+            execute = 0
+            if 'setup' in elem:
+                fill_values += [elem['setup']]
+                hatches += [hatch_dic['setup']]
+                colors += [_colors[i_elem]]
+            if 'offload' in elem:
+                fill_values += [elem['offload']]
+                hatches += [hatch_dic['offload']]
+                colors += [_colors[i_elem]]
+            if '_' in elem:
+                execute += elem['_']
+            if 'execute' in elem:
+                execute += elem['execute']
+            if execute > 0:
+                fill_values += [execute]
+                hatches += [hatch_dic['execute']]
+                colors += [_colors[i_elem]]
+
+        total = 0
+        for i, val in enumerate(fill_values):
+            bar = plt.bar(label, val, bottom=total, color=colors[i], hatch=hatches[i])
+            total += val
+
+
+    def plot(self, filename, title=None, ylabel=None, ylim=None, text=False, save=True):
         """
         Plot figure to file and configure labels and legends.
 
@@ -168,4 +239,8 @@ class StackedBarPlot:
         self.ax.legend(self.groups, ncols=4, loc='center', bbox_to_anchor=(.5, 1.08))
         self.ax.set_ylim(ylim)
 
-        plt.savefig(filename, bbox_inches='tight', pad_inches=0.1)
+        if save:
+            plt.savefig(filename, bbox_inches='tight', pad_inches=0.1)
+
+    def save(self, filename):
+        plt.savefig(filename)
