8079c6046fe07a7d36f236f073e779ec06618a2a
Johannes Bulin <johannes.bulin@ecmwf.int>
2026-02-20 08:47:29 +0000

    Fixed bugs in BashLauncher (run_dir not set, escaping of strings, ignoring non-bash compatible variable names).

--- ifsbench/launch/bashlauncher.py
+++ ifsbench/launch/bashlauncher.py
@@ -7,11 +7,13 @@
 
 from copy import deepcopy
 import datetime
+import re
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, TextIO
 
 from ifsbench.env import EnvPipeline
 from ifsbench.launch.launcher import LauncherWrapper, LaunchData
+from ifsbench.logging import debug
 
 
 class BashLauncher(LauncherWrapper):
@@ -23,12 +25,54 @@ class BashLauncher(LauncherWrapper):
     a bash script, using the path ``run_dir/bash_scripts/command_time.sh``.
     """
 
-    def _write_bash_file(self, f, launch_data):
+    def _write_header(self, f: TextIO, header: str):
+        """
+        Write a "header" to a file. A header is simply a comment of the form
+
+        ############
+        # The header
+        ###########
+        """
+        f.write("\n")
+        f.write('#' * 60)
+        f.write("\n")
+        f.write(f'# {header}')
+        f.write("\n")
+        f.write('#' * 60)
+        f.write("\n")
+        f.write("\n")
+
+    def _write_bash_file(self, f: TextIO, launch_data: LaunchData):
+        """
+        Write the content of a LaunchData object to a bash script.
+        """
         f.write("#! /bin/bash\n\n")
 
-        for key, value in launch_data.env.items():
-            f.write(f'export {key}="{value}"\n')
+        self._write_header(f, "Specify environment")
+        env_name_check = re.compile(r'[a-zA-Z_][a-zA-Z_0-9]*')
 
+        for key, value in launch_data.env.items():
+            # Check that the current "environment variable" is an actual
+            # variable. For example, bash function definitions may be in
+            # `launch_data.env` but end with `_%%` and are not proper
+            # environment variables. We skip these.
+            if not env_name_check.fullmatch(key):
+                debug(f'Ignore non-standard bash name {key} in BashLauncher.')
+                continue
+
+            # We set the environment variables inside a "..." block. This means
+            # that we have to escpae certain special characters (", $, `).
+            escaped_value = value.replace('$', '\\$')
+            escaped_value = escaped_value.replace('"', '\\"')
+            escaped_value = escaped_value.replace('`', '\\`')
+            f.write(f'export {key}="')
+            f.write(escaped_value)
+            f.write("\"\n")
+
+        self._write_header(f, "Specify launch command")
+
+        f.write(f'cd "{launch_data.run_dir}"')
+        f.write("\n")
         for c in launch_data.cmd:
             f.write(f'"{c}" ')
 
@@ -51,9 +95,6 @@ class BashLauncher(LauncherWrapper):
         time_str = datetime.datetime.now(datetime.timezone.utc).strftime(
             "%Y-%m-%d:%H-%M-%S-%f"
         )
-
-        # Derive the name of the bash script from the command. As the command
-        # may be a full path, only take the part after the last slash.
         cmd_str = cmd[0].split('/')[-1]
 
         script_path = script_dir / f"{cmd_str}_{time_str}.sh"
