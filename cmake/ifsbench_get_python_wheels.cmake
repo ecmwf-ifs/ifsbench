9d81e1bc830ac7aa9bac1e1765a170cf6a10c7cb
Balthasar Reuter <6384870+reuterbal@users.noreply.github.com>
2026-06-03 13:55:33 +0200

    Apply suggestions from code review
    Co-authored-by: johannesbulin <141238457+johannesbulin@users.noreply.github.com>
    Co-authored-by: Balthasar Reuter <6384870+reuterbal@users.noreply.github.com>

--- cmake/ifsbench_get_python_wheels.cmake
+++ cmake/ifsbench_get_python_wheels.cmake
@@ -1,4 +1,4 @@
-# (C) Copyright 2024- ECMWF.
+# (C) Copyright 2020- ECMWF.
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -10,7 +10,6 @@ cmake_minimum_required( VERSION 3.19 FATAL_ERROR )
 list( APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}" )
 include( ifsbench_python_macros )
 
-set( WHEELS_DIR "${CMAKE_CURRENT_BINARY_DIR}/wheels" CACHE PATH "" )
 set( WHEELS_DIR "${CMAKE_CURRENT_BINARY_DIR}/wheels" CACHE PATH "" )
 set( REQUIREMENT_SPEC "${CMAKE_CURRENT_LIST_DIR}/.." CACHE STRING "" )
 set( IFSBENCH_WHEEL_ARCH NONE CACHE STRING "" )
